5e9d671a5130bfdf35ab37e3603fe3c08fb4c65a
Felipe Paravidino <110398569+Felpo-1@users.noreply.github.com>
2026-04-21 22:42:28 -0300

    Commit final

--- guilda/src/main/java/br/edu/infnet/guilda/aventura/domain/ClasseAventureiro.java
+++
@@ -1,4 +0,0 @@
-package br.edu.infnet.guilda.aventura.domain;
-
-
-
--- guilda/src/main/java/br/edu/infnet/guilda/aventura/domain/EspecieCompanheiro.java
+++
@@ -1,8 +0,0 @@
-package br.edu.infnet.guilda.aventura.domain;
-
-public enum EspecieCompanheiro {
-    LOBO,
-    CORUJA,
-    GOLEM,
-    DRAGAO_MINIATURA
-}
